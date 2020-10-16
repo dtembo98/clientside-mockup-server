f535bedb5f6f3b5683e1a0c5e8083ede8061551f
David Tembo <dtembo98@Davids-MacBook-Air.local>
2020-10-16 17:52:14 +0200

    made a few changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,20 @@
-
+const jwt = require('jsonwebtoken')
 
 exports.getLogin = (req,res,next) =>
 {
     res.status(200).json({
-    status: "success"
+    status: "success",
+    name:"david",
+    accessToken:'ftGs25tFRHdhghyddggyhyhjyddgtghtgtfvj'
     })
 }
 
 exports.postLogin = (req,res,next) =>
 {
-    const data = req.body;
-    return res.status(200).json({
-    status:"success",
-    data
+   
+    res.status(200).json({
+    status: "success",
+    name:"david",
+    accessToken:'ftGs25tFRHdhghyddggyhyhjyddgtghtgtfvj'
     })
 }
